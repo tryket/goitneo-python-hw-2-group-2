cff841214f9068f884e7c4878513ffbc6f20f94c
Stryk Anton <anton.stryk@fuib.com>
2023-10-28 14:00:58 +0300

    goitneo-python-hw-2-group-2

---
+++ main.py
@@ -0,0 +1,59 @@
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Phone(Field):
+    def __init__(self, phone):
+        if not phone.isdigit() or len(phone) != 10:
+            raise ValueError("Phone number must be 10 digits.")
+        super().__init__(phone)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        try:
+            self.phones.append(Phone(phone))
+        except ValueError as e:
+            return str(e)
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if phone.value == old_phone:
+                try:
+                    phone.value = Phone(new_phone).value
+                except ValueError as e:
+                    return str(e)
+        return "Phone edited."
+
+    def find_phone(self, phone):
+        return next((p for p in self.phones if p.value == phone), None)
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+
+class AddressBook:
+    def __init__(self):
+        self.data = {}
+
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name, None)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
